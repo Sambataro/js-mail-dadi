d3802adf5070b06460aaae7da3119954712c7226
fortunato <sambataro1990@gmail.com>
2020-11-12 17:30:04 +0100

    esercizio mail completato

--- mail/js/mail.js
+++ mail/js/mail.js
@@ -1,13 +1,23 @@
-// Chiedi all’utente la sua email,
-var emailUtente = prompt("inserisci la tua email");
-console.log(emailUtente);
-// controlla che sia nella lista di chi può accedere,
-var emailUtente = mailLogin = ["s.fortunato90@gmail.com", "pincopallo0@gmail.com", "boolean@gmail.com", "atom@gmail.com"];
-console.log(mailLogin);
 
-if (true) {
-  document.getElementById("accesso").innerHTML = "accesso consentito"
+function accessoemail() {
+  // controlla che sia nella lista di chi può accedere,
+  var mailingList= ["s.fortunato90@gmail.com", "pincopallo0@gmail.com", "boolean@gmail.com"];
+  console.log(mailingList);
+  // Chiedi all’utente la sua email,
+  var emailUtente = prompt("inserisci la tua email");
+  console.log(emailUtente);
+
+  var result = false;
+  for (var i = 0; i < mailingList.length; i = i + 1) {
+    if (emailUtente == mailingList[i]) {
+      result = true;
+    }
+  }
+
+  if (result === true) {
+    document.getElementById("accesso").innerHTML = "Benvenuto: " + emailUtente;
+  } else {
+    alert("accesso non consentito");
+    document.getElementById("accesso").innerHTML = "Utente non registrato";
+  }
 }
-
-
-// stampa un messaggio appropriato sull’esito del controllo.
